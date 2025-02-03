feb9d4963d454f42a25c5fd47f3913c707bd111c
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-02-03 16:20:32 +0900

    update: 表示文関連

--- lib/dnclc.py
+++ lib/dnclc.py
@@ -1,15 +1,15 @@
 import sys
 
-
 def compile_dncl_to_llvm(dncl_code):
     llvm_code = """
-@.str = private unnamed_addr constant [4 x i8] c"%d\\0A\\00", align 1
 declare i32 @printf(i8*, ...)
 
 define i32 @main() {
 entry:
 """
     variables = {}
+    string_constants = {}
+    string_counter = 0  # 文字列定義用のカウンタ
 
     lines = dncl_code.split("\n")
     for line in lines:
@@ -25,12 +25,30 @@ entry:
             llvm_code += f"  store i32 {value}, i32* %{var_name}, align 4\n"
 
         elif "を表示する" in line:
-            var_names = [tok for tok in tokens if tok in variables]
-            for var in var_names:
-                llvm_code += f"  %{var}_val = load i32, i32* %{var}, align 4\n"
-                llvm_code += f"  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %{var}_val)\n"
+            display_tokens = line.replace("を表示する", "").strip().split("と")
+            for tok in display_tokens:
+                tok = tok.strip()
+                if tok in variables:  # 変数の場合
+                    llvm_code += f"  %{tok}_val = load i32, i32* %{tok}, align 4\n"
+                    llvm_code += f"  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.int_format, i32 0, i32 0), i32 %{tok}_val)\n"
+                elif tok.startswith("「") and tok.endswith("」"):  # 文字列リテラルの場合
+                    clean_str = tok[1:-1]  # 「」 を削除
+                    utf8_bytes = clean_str.encode('utf-8')  # UTF-8 のバイト列
+                    byte_len = len(utf8_bytes) + 2  # 改行 `\0A` + 終端 `\00`
+                    str_var = f"@.str{string_counter}"
+                    string_constants[str_var] = utf8_bytes.decode('utf-8')  # Python 文字列に戻す
+                    llvm_code += f"  call i32 (i8*, ...) @printf(i8* getelementptr ([{byte_len} x i8], [{byte_len} x i8]* {str_var}, i32 0, i32 0))\n"
+                    string_counter += 1
 
     llvm_code += "  ret i32 0\n}\n"
+
+    # 文字列定数を上部に追加
+    string_defs = '\n'.join([f'{var} = private unnamed_addr constant [{len(val) + 2} x i8] c"{val}\\0A\\00", align 1' for var, val in string_constants.items()])
+    llvm_code = string_defs + "\n" + llvm_code
+
+    # 数値フォーマット定義
+    llvm_code = '@.int_format = private unnamed_addr constant [4 x i8] c"%d\\0A\\00", align 1\n' + llvm_code
+
     return llvm_code
 
 
@@ -50,5 +68,4 @@ if __name__ == "__main__":
     with open(output_filename, "w", encoding="utf-8") as f:
         f.write(llvm_code)
 
-    print(f"Compiled {input_filename} to {output_filename}")
-    print(f"Run `clang -o program {output_filename} && ./program` to execute.")
+    print(f"Compiled completed.")
--- test/test.ll
+++ test/test.ll
@@ -1,5 +1,6 @@
+@.int_format = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
+@.str0 = private unnamed_addr constant [9 x i8] c"こんにちは世界\0A\00", align 1
 
-@.str = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
 declare i32 @printf(i8*, ...)
 
 define i32 @main() {
@@ -9,8 +10,9 @@ entry:
   %y = alloca i32, align 4
   store i32 20, i32* %y, align 4
   %x_val = load i32, i32* %x, align 4
-  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %x )
+  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.int_format, i32 0, i32 0), i32 %x_val)
   %y_val = load i32, i32* %y, align 4
-  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %y )
+  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.int_format, i32 0, i32 0), i32 %y_val)
+  call i32 (i8*, ...) @printf(i8* getelementptr ([23 x i8], [23 x i8]* @.str0, i32 0, i32 0))
   ret i32 0
 }
